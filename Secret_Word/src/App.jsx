5731108188369ccdb1147d0e083ac33b22bae07b
Andre <andrevitoreur@gmail.com>
2022-11-03 13:36:28 +0000

    Structure Select Word

--- Secret_Word/src/App.jsx
+++ Secret_Word/src/App.jsx
@@ -19,9 +19,42 @@ function App() {
   const [GameStage, setGameStage] = useState(stages[0].name)
   const [word] = useState(wordsList)
 
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [pickedWord, setPickedWord] = useState("")
+  const [letters, setLetters] = useState([])
+
+  const pickWordAndCategory = () =>{
+    //picked a random category
+    const categories = Object.keys(word);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+  
+    console.log(category)
+
+    //Pick a random Word 
+    const words = word[category][Math.floor(Math.random()* word[category].length)]
+
+    console.log(words)
+
+    return {words, category}
+   }
+
+
  //Starts the secret Word game 
  const startGame = () =>{
+  const {words, category} = pickWordAndCategory()
+  
   setGameStage(stages[1].name)
+
+  //Create an array of Letters 
+  let wordLetters = words.split("")
+
+  wordLetters = wordLetters.map((Letter) => Letter.toLowerCase())
+  console.log(wordLetters)
+
+  //Filĺ States 
+  setPickedWord(words)
+  setPickedCategory(category)
+  setLetters(wordLetters)
  }
 
  //process the letter input
