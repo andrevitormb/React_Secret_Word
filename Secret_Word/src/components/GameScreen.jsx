0282c9f6160140bb8c490b6dfd62a872e6f77a91
Andre <andrevitoreur@gmail.com>
2022-11-03 13:45:19 +0000

    Structuring GameScreen

--- Secret_Word/src/components/GameScreen.jsx
+++ Secret_Word/src/components/GameScreen.jsx
@@ -3,8 +3,30 @@ import '../styles/components/gamescreen.sass'
 const GameScreen = ({verifyLetter}) => {
   return (
     <div id='GameScreen'>
-        <h1>Game</h1>
-        <button onClick={verifyLetter}>Start Game</button>
+        <p className='points'>
+          <span>Points: 000</span>
+        </p>
+        <h1>Guess the Word</h1>
+        <h3 className='tip'>
+          Tip about the word: <span> Tip...</span>
+        </h3>
+        <div className='wordContainer'>
+          <span className='letter'>A</span>
+          <span className='blankSquare'></span>
+        </div>
+        <div className="letterContainer">
+          <p>Try guess a letter of the word </p>
+          <form>
+            <input type="text" name='letter' maxLength="1" required />
+            <button>Play!</button>
+          </form>
+        </div>
+        <div className="wrongLettersContainer">
+          <p>Letters already used</p>
+          <span>a,</span>
+          <span>b,</span>
+        </div>
+        {/* <button onClick={verifyLetter}>Start Game</button> */}
     </div>
   )
 }
